96cc0bba872258d88b9baf4646d1aa10a67923e8
zverev <alexander.zverev@gmail.com>
2014-10-23 13:29:31 +0400

    asks for a project name

--- app/index.js
+++ app/index.js
@@ -2,6 +2,16 @@ var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
     createProjectFile: function() {
-        this.copy('project.sublime-project');
+        var done = this.async();
+        this.prompt({
+            type: 'input',
+            name: 'name',
+            message: 'Your project name',
+            default: this.appname // Default to current folder name
+        }, function(answers) {
+            this.log(answers.name);
+            this.copy('project.sublime-project', answers.name + '.sublime-project');
+            done();
+        }.bind(this));
     }
 });
