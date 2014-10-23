3f4670b49739d1e692d87c299115dffb787d2603
zverev <alexander.zverev@gmail.com>
2014-10-23 12:41:36 +0400

    copies project file

--- app/index.js
+++ app/index.js
@@ -1,10 +1,7 @@
 var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
-    method1: function() {
-        console.log('foo');
-    },
-    method2: function() {
-        console.log('bar');
+    createProjectFile: function() {
+        this.copy('project.sublime-project');
     }
 });
